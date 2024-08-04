96ebac09e5493f46c9aa8ab1461c7ed43350434a
mBarneto <4347791+m-barneto@users.noreply.github.com>
2024-08-04 02:08:15 -0400

    Updated types

---
+++ src/QuestControllerExtension.ts
@@ -0,0 +1,86 @@
+import { QuestController } from "@spt/controllers/QuestController";
+import { DialogueHelper } from "@spt/helpers/DialogueHelper";
+import { ItemHelper } from "@spt/helpers/ItemHelper";
+import { ProfileHelper } from "@spt/helpers/ProfileHelper";
+import { QuestConditionHelper } from "@spt/helpers/QuestConditionHelper";
+import { QuestHelper } from "@spt/helpers/QuestHelper";
+import { TraderHelper } from "@spt/helpers/TraderHelper";
+import { IPmcData } from "@spt/models/eft/common/IPmcData";
+import { IQuest } from "@spt/models/eft/common/tables/IQuest";
+import { IItemEventRouterResponse } from "@spt/models/eft/itemEvent/IItemEventRouterResponse";
+import { IAcceptQuestRequestData } from "@spt/models/eft/quests/IAcceptQuestRequestData";
+import { ILogger } from "@spt/models/spt/utils/ILogger";
+import { EventOutputHolder } from "@spt/routers/EventOutputHolder";
+import { ConfigServer } from "@spt/servers/ConfigServer";
+import { DatabaseService } from "@spt/services/DatabaseService";
+import { LocaleService } from "@spt/services/LocaleService";
+import { LocalisationService } from "@spt/services/LocalisationService";
+import { MailSendService } from "@spt/services/MailSendService";
+import { PlayerService } from "@spt/services/PlayerService";
+import { SeasonalEventService } from "@spt/services/SeasonalEventService";
+import { ICloner } from "@spt/utils/cloners/ICloner";
+import { HttpResponseUtil } from "@spt/utils/HttpResponseUtil";
+import { TimeUtil } from "@spt/utils/TimeUtil";
+import { inject, injectable } from "tsyringe";
+import { Mod } from "./mod";
+import { ConfigTypes } from "@spt/models/enums/ConfigTypes";
+
+
+
+@injectable()
+export class QuestControllerExtension extends QuestController {
+    constructor(
+        @inject("PrimaryLogger") logger: ILogger, 
+        @inject("TimeUtil") timeUtil: TimeUtil, 
+        @inject("HttpResponseUtil") httpResponseUtil: HttpResponseUtil, 
+        @inject("EventOutputHolder") eventOutputHolder: EventOutputHolder, 
+        @inject("DatabaseService") databaseService: DatabaseService, 
+        @inject("ItemHelper") itemHelper: ItemHelper, 
+        @inject("DialogueHelper") dialogueHelper: DialogueHelper, 
+        @inject("MailSendService") mailSendService: MailSendService, 
+        @inject("ProfileHelper") profileHelper: ProfileHelper, 
+        @inject("TraderHelper") traderHelper: TraderHelper, 
+        @inject("QuestHelper") questHelper: QuestHelper, 
+        @inject("QuestConditionHelper") questConditionHelper: QuestConditionHelper, 
+        @inject("PlayerService") playerService: PlayerService, 
+        @inject("LocaleService") localeService: LocaleService, 
+        @inject("SeasonalEventService") seasonalEventService: SeasonalEventService, 
+        @inject("LocalisationService") localisationService: LocalisationService, 
+        @inject("ConfigServer") configServer: ConfigServer,
+        @inject("PrimaryCloner") cloner: ICloner
+    ) {
+        super(
+            logger,
+            timeUtil,
+            httpResponseUtil,
+            eventOutputHolder,
+            databaseService,
+            itemHelper,
+            dialogueHelper,
+            mailSendService,
+            profileHelper,
+            traderHelper,
+            questHelper,
+            questConditionHelper,
+            playerService,
+            localeService,
+            localisationService,
+            configServer,
+            cloner
+        );
+    }
+
+    // override acceptQuest(pmcData: IPmcData, acceptedQuest: IAcceptQuestRequestData, sessionID: string): IItemEventRouterResponse {
+    //     const response = super.acceptQuest(pmcData, acceptedQuest, sessionID);
+
+    //     //updateProfileTaskConditionCounterValue
+    //     const quest: IQuest = this.questHelper.getQuestFromDb(acceptedQuest.qid, pmcData);
+    //     quest.conditions.AvailableForFinish.forEach(condition => {
+    //         if (typeof condition.value === "number" && condition.id in Mod.conditionsToSkip) {
+    //             this.updateProfileTaskConditionCounterValue(pmcData, condition.id, acceptedQuest.qid, condition.value);
+    //         }
+    //     });
+
+    //     return response;
+    // }
+}
--- src/mod.ts
+++ src/mod.ts
@@ -10,73 +10,109 @@ import { ILocation } from "@spt/models/eft/common/ILocation";
 import { jsonc } from "jsonc";
 import { ILogger } from "@spt/models/spt/utils/ILogger";
 import { IQuest } from "@spt/models/eft/common/tables/IQuest";
-import { StaticRouterModService } from "@spt/services/mod/staticRouter/StaticRouterModService";
 import { getZones, Zones } from "./ZoneManager";
+import { IPreSptLoadMod } from "@spt/models/external/IPreSptLoadMod";
+import { QuestControllerExtension } from "./QuestControllerExtension";
+import { ConfigServer } from "@spt/servers/ConfigServer";
+import { ConfigTypes } from "@spt/models/enums/ConfigTypes";
 
 interface ModConfig {
     RemoveQuestsOnMaps: string[];
 }
 
-class Mod implements IPostDBLoadMod {
+class Mod implements IPreSptLoadMod { //IPostDBLoadMod
+
     private modConfig: ModConfig;
     private logger: ILogger;
     private zones: Zones;
+    private locale: Record<string, string>;
+    private maps: ILocations;
+    public static conditionsToSkip: string[];
 
-    public postDBLoad(container: DependencyContainer): void {
-        const vfs = container.resolve<VFS>("VFS");
-        this.modConfig = jsonc.parse(vfs.readFile(path.resolve(__dirname, "../config/config.jsonc")));
-
+    preSptLoad(container: DependencyContainer): void {
         this.logger = container.resolve<ILogger>("WinstonLogger");
-
-        this.zones = getZones(vfs, this.logger);
-
-        const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
-        const tables: IDatabaseTables = databaseServer.getTables();
-        const maps: ILocations = tables.locations;
-
-        const mapIdsToRemove: string[] = [];
-
-        // Loop through all the maps we have in config and make sure they exist...
-        this.modConfig.RemoveQuestsOnMaps.forEach(mapName => {
-            if (!(mapName in maps)) {
-                this.logger.error(`Unable to find map ${mapName}! Make sure you're spelling the map id correctly, see the config for valid entries.`);
-                return;
-            }
-            mapIdsToRemove.push((maps[mapName] as ILocation).base._Id);
-        });
-        this.logger.success("Found all maps in config!");
-
-        const quests: Record<string, IQuest> = tables.templates.quests;
-
-        // Loop through quests
-        for (const questId in quests) {
-            const quest: IQuest = quests[questId];
-            //this.logger.success(`Quest: ${quest.QuestName}`);
-            if (true) { //|| quest.location === "any" || mapIdsToRemove.includes(quest.location)
-                //this.logger.success(`raaaa ${quest.location}`);
-                quest.conditions.AvailableForFinish.forEach(condition => {
-                    this.logger.success(condition.type);
-                    if (condition.zoneId) {
-                        if (!this.zoneExists(condition.zoneId)) {
-                            this.logger.success(`Missing: ${condition.zoneId}`);
-                            quest.conditions.AvailableForFinish.forEach(condition => {
-                                this.logger.error(`${quest.QuestName}:${condition.conditionType} | ${condition.zoneId}`);
-                            });
-                        }
-                    }
-                });
-            }
-        }
+        container.register<QuestControllerExtension>("QuestControllerExtension", QuestControllerExtension);
+        container.register("QuestController", {useToken: "QuestControllerExtension"});
     }
 
-    zoneExists(zone: string) {
-        const mapNames = Object.keys(this.zones);
-        for (const i in mapNames) {
-            const mapName = mapNames[i];
-            if (this.zones[mapName].includes(zone)) return true;
-        }
-        return false;
-    }
+    // public postDBLoad(container: DependencyContainer): void {
+    //     const vfs = container.resolve<VFS>("VFS");
+    //     this.modConfig = jsonc.parse(vfs.readFile(path.resolve(__dirname, "../config/config.jsonc")));
+
+    //     this.zones = getZones(vfs, this.logger);
+
+    //     const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
+    //     const tables: IDatabaseTables = databaseServer.getTables();
+    //     this.maps = tables.locations;
+    //     this.locale = tables.locales.global["en"];
+
+    //     const mapIdsToRemove: string[] = [];
+
+    //     // Loop through all the maps we have in config and make sure they exist...
+    //     this.modConfig.RemoveQuestsOnMaps.forEach(mapName => {
+    //         if (!(mapName in this.maps)) {
+    //             this.logger.error(`Unable to find map ${mapName}! Make sure you're spelling the map id correctly, see the config for valid entries.`);
+    //             return;
+    //         }
+    //         mapIdsToRemove.push((this.maps[mapName] as ILocation).base._Id);
+    //     });
+    //     this.logger.success("Found all maps in config!");
+
+
+    //     Mod.conditionsToSkip = [];
+    //     const quests: Record<string, IQuest> = tables.templates.quests;
+    //     // Loop through quests
+    //     for (const questId in quests) {
+    //         const quest: IQuest = quests[questId];
+    //         //this.logger.success(`Quest: ${quest.QuestName}`);
+    //         quest.conditions.AvailableForFinish.forEach(condition => {
+    //             // if (condition.zoneId) {
+    //             //     if (!this.zoneExists(condition.zoneId)) {
+    //             //         this.logger.success(`Missing: ${condition.zoneId}`);
+    //             //     }
+    //             // }
+    //             const conditionText = this.locale[condition.id];
+    //             const conditionMap = this.getMapIdFromString(conditionText);
+    //             if (conditionMap) {
+    //                 if (conditionMap in mapIdsToRemove) {
+    //                     Mod.conditionsToSkip.push(condition.id);
+    //                 }
+    //             }
+    //         });
+    //     }
+    // }
+
+    // zoneExists(zone: string) {
+    //     const mapNames = Object.keys(this.zones);
+    //     for (const i in mapNames) {
+    //         const mapName = mapNames[i];
+    //         if (this.zones[mapName].includes(zone)) return true;
+    //     }
+    //     return false;
+    // }
+
+    // getMapIdFromString(text: string) {
+    //     const sep = text.split("on ");
+    //     text = sep[sep.length - 1];
+    //     //let foundMaps = [];
+
+    //     const entries = Object.entries(this.maps);
+    //     entries.forEach(([key, value]) => {
+    //         if (key == "base" || key == "hideout" || key == "factory4_night" || key == "terminal") return;
+    //         const map: ILocation = value;
+    //         if (text.toLowerCase().indexOf(map.base.Name.toLowerCase()) !== -1) {
+    //             return map.base.Id;
+    //         }
+    //     });
+        
+    //     // if (foundMaps.length > 1) {
+    //     //     this.logger.error(`MORE THAN 1 MAP FOUND IN ${text}`);
+    //     //     foundMaps.forEach(val => {
+    //     //         this.logger.error(val);
+    //     //     })
+    //     // }
+    //     return undefined;
+    // }
 }
 
 export const mod = new Mod();
